c70b5d53a367ac1f02dbdaaf001d85d6d43f8b3e
taaaha18 <taaaha18@gmail.com>
2025-08-26 00:24:06 +0500

    now passwords are in .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,26 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-//import { UsersModule } from './users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',   
-      port: 5432,         
-      username: 'postgres', 
-      password: '123454321',
-      database: 'Business',  
-      autoLoadEntities: true,
-      synchronize: true, 
+    ConfigModule.forRoot({
+      isGlobal: true, 
     }),
-    //UsersModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: false,
+      }),
+    }),
   ],
 })
 export class AppModule {}
